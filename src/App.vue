3f10c7b83a6db6140f5f6f2b01b5b97c8177c0ae
leodini <leodini@hotmail.com>
2020-01-03 03:36:02 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <LinkList/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LinkList from './components/LinkList.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    LinkList
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+ #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  }
 </style>
---
+++ src/components/Stats.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="">
+        <h1>A different component</h1>
+        <p>There are currently {{ countLinks }} links</p>
+        <button v-on:click="removeAllLinks">Remove all links</button>
+        <p>{{ msg }}</p>
+    </div>
+</template>
+
+<script>
+import { mapGetters, mapMutations, mapActions } from 'vuex'
+
+export default {
+    name: 'Stats',
+    data() {
+        return {
+            msg: ''
+        }
+    },
+    computed: {
+        ...mapGetters([
+            'countLinks'
+        ])
+    },
+    methods: {
+        ...mapMutations(['REMOVE_ALL']),
+        ...mapActions(['removeAll']),
+        removeAllLinks() {
+            this.removeAll().then(() => {
+                this.msg = 'Links removed'
+            })
+        }
+    }
+}
+</script>
+
+<style scoped>
+    button{
+        padding: 10px;
+        margin-top: 30px;
+        width: 100%;
+        background: none;
+        border: 1px solid lightgrey;
+        outline: none;
+        cursor: pointer;
+    }
+</style>
